222042d95e7c3a42a4648b6ac6452ebfc3c201f1
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2019-01-23 22:41:50 -0600

    Add event-handler

--- js/barChart.js
+++ js/barChart.js
@@ -1,4 +1,11 @@
 var ctx = document.getElementById('myChart').getContext('2d');
+
+var original = Chart.defaults.global.legend.onClick;
+Chart.defaults.global.legend.onClick = function(e, legendItem) {
+    update_caption(legendItem);
+    original.call(this, e, legendItem);
+}
+
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
@@ -8,9 +15,28 @@ var myChart = new Chart(ctx, {
       data: [12, 19, 3, 17, 6, 3, 7],
       backgroundColor: "rgba(153, 255, 51, 0.6)"
     }, {
-      label: 'bananas',
+      label: 'oranges',
       data: [2, 29, 5, 5, 2, 3, 10],
       backgroundColor: "rgba(255, 153, 0, 0.6)"
     }]
   }
 });
+
+var labels = {
+  "apples": true,
+  "oranges": true
+}
+
+var caption = document.getElementById("caption");
+
+var update_caption = function(legend) {
+  labels[legend.text] = legend.hidden;
+
+  var selected = Object.keys(labels).filter(function(key) {
+    return labels[key];
+  });
+
+  var text = selected.length ? selected.join(" & ") : "nothing";
+
+  caption.innerHTML = "The chart is displaying " + text;
+}
